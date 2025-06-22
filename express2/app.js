78bad36f1adde748e32975e602f9aa85d1af9451
SF <103726899+SF0629@users.noreply.github.com>
2025-06-22 18:14:30 +0900

    Update app.js

--- express2/app.js
+++ express2/app.js
@@ -5,10 +5,10 @@ const PORT = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.get('/profile', (req, res) => {
-    res.render('profile/profile', {name: '김정원', age: 17, job: '개발자'});
+app.get('/', (req, res) => {
+    res.render('profile', {name: '김정원', age: 17, job: '개발자'});
 });
 
 app.listen(PORT, () => {    
-    console.log(`서버 실행중:  http://localhost:${PORT}/profile`);
+    console.log(`서버 실행중:  http://localhost:${PORT}`);
 });
