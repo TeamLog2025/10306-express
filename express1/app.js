7bc74ed3daa69bda179f1cbca26fe9dd0b753adf
SF <103726899+SF0629@users.noreply.github.com>
2025-06-22 17:48:06 +0900

    Create app.js

---
+++ express1/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(PORT, () => {    
+    console.log(`서버 실행중:  http://localhost:${PORT}`);
+});
